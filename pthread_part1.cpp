800ae831f4cbdfb670645614eb461cf7d72d76b1
yu100789 <yu1997861250@hotmail.com>
2020-04-10 02:19:22 +0800

    Global

--- pthread_part1.cpp
+++ pthread_part1.cpp
@@ -12,7 +12,6 @@
 #include <unistd.h>
 #include <algorithm>
 
-
 #define CORE_NUM 4
 #define MATRIX_SIZE 2000
 using namespace std;
@@ -21,8 +20,7 @@ Compiler: g++ -pthread pthread_part1.cpp -o pthread_part1.out
 Execute	: ./pthread_part1.out
 ******************************************************/
 
-pthread_mutex_t count_mutex;	// pthread lock variable
-
+pthread_mutex_t count_mutex; // pthread lock variable
 
 struct Thread_Data
 {
@@ -31,192 +29,224 @@ struct Thread_Data
 	int Total_Size;
 	int Thread_ID;
 	int Core;
-	float** Input_Matrix;
-	float** Output_Matrix;	
+	float **Input_Matrix;
+	float **Output_Matrix;
 };
 
 void Single_Matrix_Multiplication(Thread_Data Thread);
-void* Global_Multi_Matrix_Multiplication(void *args);
-void* Partition_Multi_Matrix_Multiplication(void *args);
+void *Global_Multi_Matrix_Multiplication(void *args);
+void *Partition_Multi_Matrix_Multiplication(void *args);
 void Print_Thread_Data(Thread_Data Thread);
-void Compare_Result(float** Matrix1, float** Matrix2, int Matrix_Size);
+void Compare_Result(float **Matrix1, float **Matrix2, int Matrix_Size);
 
-void Set_CPU( int CPU_NUM );
+void Set_CPU(int CPU_NUM);
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 	int Num_Thread = CORE_NUM;
 	string line;
 	struct timeval start;
-    struct timeval end;
+	struct timeval end;
 	double Time_Use;
-	
-/********************************** Matrix Initialize ************************************************************
+
+	/********************************** Matrix Initialize ************************************************************
 ******************************************************************************************************************/
 	float **Input_Matrix;
 	float **Single_Output_Matrix;
 	float **Multi_Output_Matrix;
 
-	Input_Matrix = new float*[MATRIX_SIZE];
-	for( int i = 0; i < MATRIX_SIZE; i++){
+	Input_Matrix = new float *[MATRIX_SIZE];
+	for (int i = 0; i < MATRIX_SIZE; i++)
+	{
 		Input_Matrix[i] = new float[MATRIX_SIZE];
 	}
 
-	Single_Output_Matrix = new float*[MATRIX_SIZE];
-	for( int i = 0; i < MATRIX_SIZE; i++){
+	Single_Output_Matrix = new float *[MATRIX_SIZE];
+	for (int i = 0; i < MATRIX_SIZE; i++)
+	{
 		Single_Output_Matrix[i] = new float[MATRIX_SIZE];
 	}
 
-	Multi_Output_Matrix = new float*[MATRIX_SIZE];
-	for( int i = 0; i < MATRIX_SIZE; i++){
+	Multi_Output_Matrix = new float *[MATRIX_SIZE];
+	for (int i = 0; i < MATRIX_SIZE; i++)
+	{
 		Multi_Output_Matrix[i] = new float[MATRIX_SIZE];
 	}
 
 	//Initial Matrix Value
-	for( int i = 0; i < MATRIX_SIZE; i++){
-		for( int j = 0; j < MATRIX_SIZE; j++){	
-			Input_Matrix[i][j] = ((float) rand()/ (RAND_MAX));
+	for (int i = 0; i < MATRIX_SIZE; i++)
+	{
+		for (int j = 0; j < MATRIX_SIZE; j++)
+		{
+			Input_Matrix[i][j] = ((float)rand() / (RAND_MAX));
 		}
 	}
-/********************************** Single Thread Matrix Multiplication *********************************************
-******************************************************************************************************************/	
-	Thread_Data Single_Thread_Data;	
+	/********************************** Single Thread Matrix Multiplication *********************************************
+******************************************************************************************************************/
+	Thread_Data Single_Thread_Data;
 	Single_Thread_Data.Input_Matrix = Input_Matrix;
 	Single_Thread_Data.Output_Matrix = Single_Output_Matrix;
 	Single_Thread_Data.Start = 0;
 	Single_Thread_Data.End = MATRIX_SIZE;
 	Single_Thread_Data.Total_Size = MATRIX_SIZE;
-	
-	cout << "\n===========Start Single Thread Matrix Multiplication===========" << endl; 
+
+	cout << "\n===========Start Single Thread Matrix Multiplication===========" << endl;
 	gettimeofday(&start, NULL);
 	Single_Matrix_Multiplication(Single_Thread_Data);
 	gettimeofday(&end, NULL);
-	Time_Use = (end.tv_sec - start.tv_sec) + (double)(end.tv_usec - start.tv_usec)/1000000.0;
+	Time_Use = (end.tv_sec - start.tv_sec) + (double)(end.tv_usec - start.tv_usec) / 1000000.0;
 	cout << "Single Thread Spend time : " << Time_Use << endl;
 
-	//Print_Thread_Data(Single_Thread_Data);
-/********************************** Global Multi Thread Matrix Multiplication ************************************
+	// Print_Thread_Data(Single_Thread_Data);
+	/********************************** Global Multi Thread Matrix Multiplication ************************************
 ******************************************************************************************************************/
-	Thread_Data Multi_Thread_Data[CORE_NUM];	
+	Thread_Data Multi_Thread_Data[CORE_NUM];
 	/*~~~~~~~~~~~~~~~Your code~~~~~~~~~~~~~~~*/
-	
+	for (int i = 0; i < CORE_NUM; i++)
+	{
+		Multi_Thread_Data[i].Input_Matrix = Input_Matrix;
+		Multi_Thread_Data[i].Output_Matrix = Multi_Output_Matrix;
+		Multi_Thread_Data[i].Start = i * MATRIX_SIZE / CORE_NUM;
+		Multi_Thread_Data[i].End = Multi_Thread_Data[i].Start + MATRIX_SIZE / CORE_NUM - 1;
+		Multi_Thread_Data[i].Thread_ID = i;
+		Multi_Thread_Data[i].Core = i;
+		Multi_Thread_Data[i].Total_Size = MATRIX_SIZE;
+	}
 	cout << "\n===========Start Global Multi Thread Matrix Multiplication===========" << endl;
 
-	pthread_t pthread_Thread[CORE_NUM]; 
-
+	pthread_t pthread_Thread[CORE_NUM];
 
 	//Start pthread execution
 	gettimeofday(&start, NULL);
 	/*~~~~~~~~~~~~~~~Your code~~~~~~~~~~~~~~~*/
+	for (int i = 0; i < CORE_NUM; i++)
+	{
+		pthread_create(&pthread_Thread[i], NULL, Global_Multi_Matrix_Multiplication, &Multi_Thread_Data[i]);
+	}
+	for (int i = 0; i < CORE_NUM; i++)
+	{
+		pthread_join(pthread_Thread[i], NULL);
+	}
 	gettimeofday(&end, NULL);
-	Time_Use = (end.tv_sec - start.tv_sec) + (double)(end.tv_usec - start.tv_usec)/1000000.0;
+	Time_Use = (end.tv_sec - start.tv_sec) + (double)(end.tv_usec - start.tv_usec) / 1000000.0;
 	cout << "Global Multi Thread Spend time : " << Time_Use << endl;
-	//Print_Thread_Data(Multi_Thread_Data[0]);
-	
-	//cout << "\n===========Global Multi Thread Compare Result===========" << endl; 
+	// Print_Thread_Data(Multi_Thread_Data[0]);
+
+	//cout << "\n===========Global Multi Thread Compare Result===========" << endl;
 	Compare_Result(Single_Output_Matrix, Multi_Output_Matrix, MATRIX_SIZE);
 
-/********************************** Partition Multi Thread Matrix Multiplication ************************************
+	/********************************** Partition Multi Thread Matrix Multiplication ************************************
 ******************************************************************************************************************/
 	/*~~~~~~~~~~~~~~~Your code~~~~~~~~~~~~~~~*/
-	
+
 	cout << "\n===========Start Partition Multi Thread Matrix Multiplication===========" << endl;
 
 	//Start pthread execution
 	gettimeofday(&start, NULL);
 	/*~~~~~~~~~~~~~~~Your code~~~~~~~~~~~~~~~*/
 	gettimeofday(&end, NULL);
-	Time_Use = (end.tv_sec - start.tv_sec) + (double)(end.tv_usec - start.tv_usec)/1000000.0;
+	Time_Use = (end.tv_sec - start.tv_sec) + (double)(end.tv_usec - start.tv_usec) / 1000000.0;
 	cout << "Partition Multi Thread Spend time : " << Time_Use << endl;
 	//Print_Thread_Data(Multi_Thread_Data[0]);
-	
-	//cout << "\n===========Partition Multi Thread Compare Result===========" << endl; 
+
+	//cout << "\n===========Partition Multi Thread Compare Result===========" << endl;
 	Compare_Result(Single_Output_Matrix, Multi_Output_Matrix, MATRIX_SIZE);
 
+	/********************************** Output ***********************************************************************
+******************************************************************************************************************/
 
-/********************************** Output ***********************************************************************
-******************************************************************************************************************/	
-
-	cout << "\nSuccess Execution !!" << endl; 
+	cout << "\nSuccess Execution !!" << endl;
 	//sleep(10);
 	return 0;
 }
 
-
 void Single_Matrix_Multiplication(Thread_Data Thread)
 {
-	for( int i = 0 ; i < Thread.Total_Size; i++){
-		for( int j = 0 ; j < Thread.Total_Size; j++)
+	for (int i = 0; i < Thread.Total_Size; i++)
+	{
+		for (int j = 0; j < Thread.Total_Size; j++)
 		{
 			Thread.Output_Matrix[i][j] = 0;
-			for( int k = 0 ; k < Thread.Total_Size; k++)
+			for (int k = 0; k < Thread.Total_Size; k++)
 			{
-				Thread.Output_Matrix[i][j] += Thread.Input_Matrix[i][k]*Thread.Input_Matrix[k][j];
-			}				
+				Thread.Output_Matrix[i][j] += Thread.Input_Matrix[i][k] * Thread.Input_Matrix[k][j];
+			}
 		}
 	}
 }
 
-void* Global_Multi_Matrix_Multiplication(void *args)
+void *Global_Multi_Matrix_Multiplication(void *args)
 {
-	Thread_Data *Thread = (struct Thread_Data*) args;
+	Thread_Data *Thread = (struct Thread_Data *)args;
 	int Core = sched_getcpu();
 	int PID = syscall(SYS_gettid);
-	
-	cout << "The thread " << Thread->Thread_ID << " PID : " << PID << " is on CPU" << Core << endl;	
-	
+	cout << "The thread " << Thread->Thread_ID << " PID : " << PID << " is on CPU" << Core << endl;
+
 	/*~~~~~~~~~~~~~~~Your code~~~~~~~~~~~~~~~*/
+	// if (Thread->Core != Core)
+	// {
+	// 	Set_CPU(Thread->Core);
+	// 	cout << "The thread " << Thread->Thread_ID << " PID : " << PID << " is moved from CPU" << Core << " to CPU" << sched_getcpu() << endl;
+	// }
+	for (int i = Thread->Start; i <= Thread->End; i++)
+	{
+		for (int j = 0; j < Thread->Total_Size; j++)
+		{
+			Thread->Output_Matrix[i][j] = 0;
+			for (int k = 0; k < Thread->Total_Size; k++)
+			{
+				Thread->Output_Matrix[i][j] += Thread->Input_Matrix[i][k] * Thread->Input_Matrix[k][j];
+			}
+		}
+	}
 }
 
-void* Partition_Multi_Matrix_Multiplication(void *args)
+void *Partition_Multi_Matrix_Multiplication(void *args)
 {
-	Thread_Data *Thread = (struct Thread_Data*) args;
+	Thread_Data *Thread = (struct Thread_Data *)args;
 	int Core = sched_getcpu();
 	int PID = syscall(SYS_gettid);
-	pthread_mutex_lock( &count_mutex );
-	cout << "The thread " << Thread->Thread_ID << " PID : " << PID << " is on CPU" << Core << endl;	
-	pthread_mutex_unlock( &count_mutex );
+	pthread_mutex_lock(&count_mutex);
+	cout << "The thread " << Thread->Thread_ID << " PID : " << PID << " is on CPU" << Core << endl;
+	pthread_mutex_unlock(&count_mutex);
 	/*~~~~~~~~~~~~~~~Your code~~~~~~~~~~~~~~~*/
 }
 
 void Print_Thread_Data(Thread_Data Thread)
 {
-	for( int i = Thread.Start ; i < Thread.Total_Size; i++){
-		for( int j = 0 ; j < Thread.Total_Size; j++)
+	for (int i = Thread.Start; i < Thread.Total_Size; i++)
+	{
+		for (int j = 0; j < Thread.Total_Size; j++)
 		{
-			cout << Thread.Output_Matrix[i][j]	<< ", " ;		
+			cout << Thread.Output_Matrix[i][j] << ", ";
 		}
 		cout << endl;
 	}
 }
 
-void Set_CPU( int CPU_NUM )
+void Set_CPU(int CPU_NUM)
 {
 	/*~~~~~~~~~~~~~~~Your code~~~~~~~~~~~~~~~*/
+	cpu_set_t set;
+	CPU_ZERO(&set);
+	CPU_SET(CPU_NUM, &set);
+	sched_setaffinity(0, sizeof(set), &set);
 }
 
-void Compare_Result(float** Matrix1, float** Matrix2, int Matrix_Size)
+void Compare_Result(float **Matrix1, float **Matrix2, int Matrix_Size)
 {
 	//Matrix 2 will be set zero
-	for( int i = 0 ; i < Matrix_Size; i++){
-		for( int j = 0 ; j < Matrix_Size; j++)
+	for (int i = 0; i < Matrix_Size; i++)
+	{
+		for (int j = 0; j < Matrix_Size; j++)
 		{
-			if( Matrix1[i][j] != Matrix2[i][j])
+			if (Matrix1[i][j] != Matrix2[i][j])
 			{
 				cout << "Result Not Same !! " << endl;
-				return ;
-			} 	
-			Matrix2[i][j] = 0;				
+				return;
+			}
+			Matrix2[i][j] = 0;
 		}
 	}
-	cout << "====Result PASS====" << endl;		
+	cout << "====Result PASS====" << endl;
 }
-
-
-
-
-
-
-
-
-
